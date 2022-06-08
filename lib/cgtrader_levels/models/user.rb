60e388450c562188a1b65dfa2d25fb1dc7a408a3
rodrigomarafelli <rodrigomarafelli@appnoz.com.br>
2022-06-08 11:25:52 -0300

    refactor: improves code logic and implements some new features

--- lib/cgtrader_levels/models/user.rb
+++ lib/cgtrader_levels/models/user.rb
@@ -1,27 +1,36 @@
+# require 'cgtrader_levels'
+require 'active_record'
+
 class CgtraderLevels::User < ActiveRecord::Base
   attr_reader :level
 
   after_initialize do
     self.reputation = 0
 
-    matching_level = CgtraderLevels::Level.where(experience: reputation).first
-
-    if matching_level
-      self.level_id = matching_level.id
-      @level = matching_level
-    end
+    set_level
   end
 
-  after_update :set_new_level
+  after_update :set_level, if: -> { self.reputation_changed? }
+  after_update :give_privileges_by_level_increase, if: -> { self.level_id_changed? }
 
   private
 
-  def set_new_level
-    matching_level = CgtraderLevels::Level.where(experience: reputation).first
+  def set_level
+    matching_level = CgtraderLevels::Level.find_by(experience: reputation)
 
-    if matching_level
-      self.level_id = matching_level.id
-      @level = matching_level
+    return unless matching_level
+
+    self.level_id = matching_level.id
+    @level = matching_level
+  end
+
+  def give_privileges_by_level_increase
+    CgtraderLevels::Privilege.where(level_id: @level.id).each do |privilege|
+      self.coins += privilege.bonus if privilege.extra_coins?
+      
+      self.tax -= privilege.bonus if privilege.tax_reduction?
+
+      self.available_3d_models_amount += privilege.bonus if privilege.extra_3d_models?
     end
   end
 end
